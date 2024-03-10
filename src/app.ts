df44ab9928d3b6bf5de9f551cef58dff8a81d225
gorilafreela <98602969+gorilafreela@users.noreply.github.com>
2024-03-10 15:11:27 -0300

    fixed cors issue

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
 import express, { Express } from "express";
 import { TaskRouter } from "./routes/task.router";
 import { CommentRouter } from "./routes/comment.router";
-
+import cors from "cors"
 import { UserRouter } from "./routes/user.router";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 export class Application {
   public express: Express;
 
   public init(): Promise<void> {
     return new Promise((resolve, _reject) => {
+   
       const taskRouter: TaskRouter = new TaskRouter();
       const commentRouter: CommentRouter = new CommentRouter();
       const userRouter: UserRouter = new UserRouter();
       this.express = express();
-      
+      this.express.use(cors());
+
       this.express.use(bodyParser.json());
       this.express.use("/tasks", taskRouter.createRoutes());
       this.express.use("/comments", commentRouter.createRoutes());
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,42 @@
-import express from 'express';
-import { Application } from "./app";
-import { config } from 'dotenv';
-import MongoDb from './database/mongodb';
+import express from "express";
+import { Application } from "./app"; // Update the file path as needed
+import { config } from "dotenv";
+import MongoDb from "./database/mongodb";
+import cors from "cors";
 
 class AssessmentNous {
   public static async start() {
-    config();
-    console.info('starting...');
-    const port = process.env.PORT;
-    const environment = process.env.NODE_ENV;
+    config(); // Load environment variables from .env file
+    console.info("Starting...");
 
-    try {
-      await MongoDb.connect();
+    await MongoDb.connect();
 
-      const app = new Application();
-      
-      await app.init();
-  
-      app.express.listen(port, () => {
-        console.info("*****************************************************************************");
-        console.info(`**                         ASSESSMENT NOUS                                 **`);
-        console.info(`** Server ${environment} started - listening on ${port}                                      **`);
-        console.info("*****************************************************************************");
-      }).on('error', (err) => {
-        if (err) {
-          return console.error(err);
-        }
-      });
-    } catch (error) {
-      console.error('Error starting AssessmentNous:', error);
-      process.exit(1); 
-    }
+    const app = new Application();
+
+    await app.init();
+
+    const port = process.env.PORT || 3030;
+
+    const environment = process.env.NODE_ENV || "development";
+
+    app.express.listen(port).on("listening", () => {
+      console.info(
+        "*****************************************************************************"
+      );
+      console.info(
+        `**                         ASSESSMENT NOUS                                 **`
+      );
+      console.info(
+        `** Server ${environment} started - listening on ${port}                                      **`
+      );
+      console.info(
+        "*****************************************************************************"
+      );
+    });
+
+    app.express.on("error", (err) => {
+      console.error("Error starting server:", err);
+    });
   }
 }
 
